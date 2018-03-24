7d2eff9bf7efdbdb097c88c3e3fb9810cdc2cab1
SSZero88 <SSZero88@gmail.com>
2018-03-24 00:57:09 -0400

    add sample html consuming xp rest endpoint

--- routes/party.js
+++ routes/party.js
@@ -13,6 +13,8 @@ router.get('/experiencebar', function(req, res, next) {
 router.get(['/','/experience'], function(req, res, next) {
     partyDataAccess.getPartyStatus((party) => {
         party.percent = math.getPercent(party.next_xp - party.prev_xp, party.current_xp - party.prev_xp);
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res.send(party);
     });
 });
